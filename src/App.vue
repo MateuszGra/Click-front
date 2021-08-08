5719307afda9d91ea24c1a936facd85ded105580
Mateusz Grabowski <m.grabowski@rolladengroup.de>
2021-08-08 11:47:52 +0200

    Add global styles, create header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,132 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Header />
+  <div class="row">
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap');
+
+$base-font-size: 20px !default;
+$hd-font-size: 18px;
+$mobile-font-size: 16px;
+
+$base-font-family: 'Roboto', sans-serif;
+$base-line-height: 1.6 !default;
+$base-margin: 1rem !default;
+
+$primary-color: #f37224;
+$secondary-color: #f6f6f6;
+$base-color: #2c3847;
+$background-color: #ffffff;
+$border-color: #ccc;
+
+html {
+  font-family: $base-font-family;
+  font-size: $base-font-size;
+  color: $base-color;
+  background: $background-color;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  overflow-x: hidden;
+  font-size: 1rem;
+  min-height: 100%;
+  &.blocked {
+    overflow: hidden;
+  }
+}
+
+@media (max-width: 1600px) {
+  html {
+    font-size: $hd-font-size;
+  }
+}
+
+@media (max-width: 1200px) {
+  html {
+    font-size: $mobile-font-size;
+  }
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  line-height: 1.2;
+  font-weight: 300;
+  color: #363a3d;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: $base-margin 0;
+}
+
+h1 {
+  font-size: 1.7rem;
+}
+
+h2 {
+  font-size: 1.4rem;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+h4 {
+  font-size: 1rem;
+}
+
+h5 {
+  font-size: 0.9rem;
+}
+
+h6 {
+  font-size: 0.8rem;
+}
+
+p {
+  font-size: 1rem;
+  line-height: 1.6;
+}
+
+.container {
+  max-width: 1140px;
+  width: 100%;
+  padding-right: 0.75rem;
+  padding-left: 0.75rem;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="row">
+        <svg class="header-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="ico_inbox">
+          <path class="eist2" d="M80.7 36.7L97 48v42.9c0 3.4-2.7 6.1-6.1 6.1H9.1C5.7 97 3 94.3 3 90.9V48l16.3-11.2" />
+          <path
+            class="eist2"
+            d="M15.3 84.7l20.4-16.3h28.6l20.4 16.3M97 48L72.5 64.3M3 48l24.5 16.3M80.7 58.9V3H19.3v55.7M31.6 15.3h8.2M39.8 27.5h28.6M31.6 39.8h36.8M31.6 52h36.8"
+          />
+        </svg>
+        <h1>Messages</h1>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  padding: 2rem 0;
+  border-bottom: 1px solid #e6e6e6;
+
+  &-icon {
+    width: 2.4rem;
+    height: 2.4rem;
+    margin: 0.4rem 1.25rem 0.4rem 0;
+    stroke-width: 5;
+  }
+}
+
+.eist2 {
+  fill: none;
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10;
+  color: #6ebe3b;
+}
+</style>
